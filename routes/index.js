af24732e239461607fb2314489147e73cfe95681
pierreolivier <bouteau.pierreolivier@gmail.com>
2014-11-29 21:14:51 +0100

    stats

--- routes/index.js
+++ routes/index.js
@@ -40,12 +40,26 @@ router.get('/player', function(req, res) {
   res.render('player', {
     title: 'Express',
     name: 'p.o',
+    steam_profile: 'https://steamcommunity.com/id/pierr/',
+    game_played: 10,
+    wins: 10,
+    losses: 10,
+    points: 10,
+    with_wins: 10,
+    with_losses: 10,
+    highlights: [
+      {image: 'images/1.png', rounds: 10, wins: 10}, {image: 'images/2.png', rounds: 10, wins: 10}, {image: 'images/4.png', rounds: 10, wins: 10}
+    ],
+    winning_spree_image: 'images/1.png',
+    winning_spree_wins: 10,
+    best_image: 'images/10.png',
+    best_score: '10 - 0',
     matches: [
       {versus: 'p.o (+x) vs DaveW (+y)', date: '11/29/2014', winner: 'p.o', rounds: [
-        {nauts: 'Lonestar vs Frog', score: '10 - 3'}, {nauts: 'Lonestar vs Frog', score: '10 - 6'}, {nauts: 'Lonestar vs Frog', score: '10 - 5'}, {nauts: 'Lonestar vs Frog', score: '10 - 5'}, {nauts: 'Lonestar vs Frog', score: '10 - 5'}
+        {rank: 1, nauts: 'Lonestar vs Frog', score: '10 - 3'}, {rank: 2, nauts: 'Lonestar vs Frog', score: '10 - 6'}, {rank: 3, nauts: 'Lonestar vs Frog', score: '10 - 5'}, {rank: 4, nauts: 'Lonestar vs Frog', score: '10 - 5'}, {rank: 5, nauts: 'Lonestar vs Frog', score: '10 - 5'}
       ]},
       {versus: 'p.o (+x) vs DaveW (+y)', date: '11/29/2014', winner: 'p.o', rounds: [
-        {nauts: 'Lonestar vs Frog', score: '10 - 5'}, {nauts: 'Lonestar vs Frog', score: '10 - 5'}, {nauts: 'Lonestar vs Frog', score: '10 - 5'}
+        {rank: 1, nauts: 'Lonestar vs Frog', score: '10 - 5'}, {rank: 2, nauts: 'Lonestar vs Frog', score: '10 - 5'}, {rank: 3, nauts: 'Lonestar vs Frog', score: '10 - 5'}
       ]}
     ]
   });
