a2f36d5fb9fc50cfa78ae308d65e554d14eac60c
pierreolivier <bouteau.pierreolivier@gmail.com>
2014-11-30 10:19:22 +0100

    fix Biggest Wining Spree

--- routes/index.js
+++ routes/index.js
@@ -7,77 +7,15 @@ var api = require('../lib/api');
 router.get('/', function(req, res) {
   api.groups(function (groups) {
     res.render('index', {
-      title: 'Express',
       groups: groups
     });
   });
-
-  /*res.render('index', {
-    title: 'Express',
-    groups: [
-      { name: 'Group 1',
-        players: [
-          {rank: 1, name: 'p.o', wins: 3, losses: 2, points: 19},
-          {rank: 1, name: 'p.o', wins: 3, losses: 2, points: 19},
-          {rank: 1, name: 'p.o', wins: 3, losses: 2, points: 19}
-        ]
-      },
-      { name: 'Group 1',
-        players: [
-          {rank: 1, name: 'p.o', wins: 3, losses: 2, points: 19},
-          {rank: 1, name: 'p.o', wins: 3, losses: 2, points: 19}
-        ]
-      },
-      { name: 'Group 1',
-        players: [
-          {rank: 1, name: 'p.o', wins: 3, losses: 2, points: 19},
-          {rank: 1, name: 'p.o', wins: 3, losses: 2, points: 19},
-          {rank: 1, name: 'p.o', wins: 3, losses: 2, points: 19}
-        ]
-      },
-      { name: 'Group 1',
-        players: [
-          {rank: 1, name: 'p.o', wins: 3, losses: 2, points: 19},
-          {rank: 1, name: 'p.o', wins: 3, losses: 2, points: 19}
-        ]
-      }
-    ]
-  });*/
 });
 
 router.get('/player', function(req, res) {
   api.player(req.query.id, function(player) {
-    //console.log(player);
     res.render('player', player);
   });
-
-  /*res.render('player', {
-    title: 'Express',
-    name: 'p.o',
-    group: 'Group 1',
-    steam_profile: 'https://steamcommunity.com/id/pierr/',
-    game_played: 10,
-    wins: 10,
-    losses: 10,
-    points: 10,
-    with_wins: 10,
-    with_losses: 10,
-    highlights: [
-      {image: 'images/1.png', rounds: 10, wins: 10}, {image: 'images/2.png', rounds: 10, wins: 10}, {image: 'images/4.png', rounds: 10, wins: 10}
-    ],
-    winning_spree_image: 'images/1.png',
-    winning_spree_wins: 10,
-    best_image: 'images/10.png',
-    best_score: '10 - 0',
-    matches: [
-      {versus: 'p.o (+x) vs DaveW (+y)', date: '11/29/2014', winner: 'p.o', rounds: [
-        {rank: 1, nauts: 'Lonestar vs Frog', score: '10 - 3'}, {rank: 2, nauts: 'Lonestar vs Frog', score: '10 - 6'}, {rank: 3, nauts: 'Lonestar vs Frog', score: '10 - 5'}, {rank: 4, nauts: 'Lonestar vs Frog', score: '10 - 5'}, {rank: 5, nauts: 'Lonestar vs Frog', score: '10 - 5'}
-      ]},
-      {versus: 'p.o (+x) vs DaveW (+y)', date: '11/29/2014', winner: 'p.o', rounds: [
-        {rank: 1, nauts: 'Lonestar vs Frog', score: '10 - 5'}, {rank: 2, nauts: 'Lonestar vs Frog', score: '10 - 5'}, {rank: 3, nauts: 'Lonestar vs Frog', score: '10 - 5'}
-      ]}
-    ]
-  });*/
 });
 
 module.exports = router;
