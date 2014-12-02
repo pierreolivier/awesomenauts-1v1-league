b46c811f47c9037aac6a51f6a2d994299a7f051a
pierreolivier <bouteau.pierreolivier@gmail.com>
2014-12-02 10:42:04 +0100

    round edit

--- public/javascripts/manager.js
+++ public/javascripts/manager.js
@@ -56,16 +56,20 @@ function getMapName(id) {
     }
 }
 
-function promptEdit(url, title, postData, name, defaultValue) {
+function promptEdit(url, title, postData, name, defaultValue, cb) {
     if (defaultValue == undefined) {
         defaultValue = '';
+    }
+    if (cb == undefined) {
+        cb = function() { location.reload() };
     }
     var value = prompt(title, defaultValue);
     if (value != null) {
         postData[name] = value;
 
         $.post(url, postData, function(data, status){
-            location.reload();
+            //location.reload();
+            cb();
         });
     }
 }
@@ -137,13 +141,31 @@ function promptAddRound(id) {
                                 map: map
                             };
                             $.post('/a/match/round/add', postData, function (data, status) {
-                                updateMatchDetail(id, function () {
-                                });
+                                updateMatchDetail(id, function () {});
                             });
                         }
                     }
                 }
             }
+        }
+    }
+}
+
+function promptEditRound(url, title, postData, name, defaultValue) {
+    promptEdit(url, title, postData, name, defaultValue, function () {
+        updateMatchDetail(postData.id, function () {});
+    });
+}
+
+function promptEditRoundScore(id) {
+    var score_player_1 = prompt('score player 1 ?');
+    if (score_player_1 != null) {
+        var score_player_2 = prompt('score player 2 ?');
+        if (score_player_2 != null) {
+            var postData = {id: id, score_player_1: score_player_1, score_player_2: score_player_2};
+            $.post('/a/match/round/edit/score', postData, function(data, status){
+                updateMatchDetail(id, function () {});
+            });
         }
     }
 }
@@ -158,12 +180,19 @@ function updateMatchDetail(id, cb) {
 
         data = JSON.parse(data);
 
+        var nauts = '';
+        for (var i = 1; i <= 20; i++) {
+            nauts += i + ': ' + getNautName(i) + '   ';
+        }
+
         for (var i in data) {
             div.append('<div class="admin_item">' +
             '<div class="admin_item_value" style="width: 1px">&nbsp;</div>'+
             '<div class="admin_item_value" style="width: 20px"><b>#' + data[i].rank + '</b></div>'+
-            '<div class="admin_item_value" style="width: 600px">' + getNautName(data[i].naut_player_1) + ' (' + getMapName(data[i].map) + ')</div>'+
-            '<div class="admin_item_value" style="width: 80px">' + data[i].score_player_1 + ' - ' + data[i].score_player_2 + '</div>' +
+            '<div class="admin_item_value" style="width: 200px">' + getNautName(data[i].naut_player_1) + '<div class="command"><a href="javascript:promptEditRound(\'/a/match/round/edit/naut\', \'naut ? ' + nauts + '\', {id: \'' + id + '\'}, \'value\')"><img width="20" src="/images/edit.png" /></a></div></div>'+
+            '<div class="admin_item_value" style="width: 200px">' + getMapName(data[i].map) + '<div class="command"><a href="javascript:promptEditRound(\'/a/match/round/edit/map\', \'map ? 0: Sorona   1: AI station 404\', {id: \'' + id + '\'}, \'value\')"><img width="20" src="/images/edit.png" /></a></div></div>'+
+            '<div class="admin_item_value" style="width: 60px; padding-right: 40px" align="right">' + data[i].picker + '<div class="command"><a href="javascript:promptEditRound(\'/a/match/round/edit/picker\', \'who picked ? (1 or 2)\', {id: \'' + id + '\'}, \'value\')"><img width="20" src="/images/edit.png" /></a></div></div>'+
+            '<div class="admin_item_value" style="width: 140px; padding-right: 40px" align="right">' + data[i].score_player_1 + ' - ' + data[i].score_player_2 + '<div class="command"><a href="javascript:promptEditRoundScore(\'' + id + '\')"><img width="20" src="/images/edit.png" /></a></div></div>' +
             '</div>');
         }
 
