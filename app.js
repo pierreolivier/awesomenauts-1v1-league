15e597f2a86b696171afecb7c2f326bf139b7172
pierreolivier <bouteau.pierreolivier@gmail.com>
2014-12-09 18:13:28 +0100

    fix leaderboard on group pages

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: 86400000 }));
 app.use(session({
     secret: configuration.server.sessionSecret,
     resave: false,
